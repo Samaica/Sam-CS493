253262c71cae9b41ec1011b94a363f950ee0dd0b
Sam <47643081+Samaica@users.noreply.github.com>
2020-01-10 11:32:08 -0500

    Update ex6.js

--- Exercise 6/JavaScript/ex6.js
+++ Exercise 6/JavaScript/ex6.js
@@ -1,1 +1,32 @@
-alert("jsdfkjlsdfjklsdf")
+//alert("JavaScript is useful!")
+/* block
+    comment*/
+
+
+// Same as a = 5; b = 10;
+/*a = 5;
+b = 10;
+c = a + b;
+alert(c);*/
+
+helloworld = "Finally, it's Friday!\n";
+helloworld2 = 'Finally it\'s "Friday"!'
+greeting = helloworld;
+//Check datatype of variables, use typeof
+
+
+console.log(greeting);
+console.log(helloworld2)
+
+document.write(greeting);
+document.write(helloworld2);
+
+count = 1;
+count += 1; //count = count + 1;
+
+console.log(count)
+
+if (count > 0)
+    console.log("It is greater than 0");
+
+    // document.getElementById("leadtext").style.color="blue";
