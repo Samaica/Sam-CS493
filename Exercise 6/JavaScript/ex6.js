c8b1736d787f114a7b659f88d0421066c3d195e8
Sam <47643081+Samaica@users.noreply.github.com>
2020-01-13 11:55:05 -0500

    Update ex6.js

--- Exercise 6/JavaScript/ex6.js
+++ Exercise 6/JavaScript/ex6.js
@@ -28,6 +28,32 @@ count += 1; //count = count + 1;
 console.log(count)
 
 if (count > 0)
-    console.log("It is greater than 0");
+    console.log("It is greater than 0\n");
 
     // document.getElementById("leadtext").style.color="blue";
+
+let age = 20;
+
+if (age > 18)
+    console.log("Old Enough");
+
+else 
+    console.log("Too Young!");
+
+let temp = 34;
+
+if (temp < 32)
+    document.write("Freezing waeather");
+
+else if (temp < 41 && temp >= 32)
+    document.write("Very Cold weather");
+
+else if (temp < 51 && temp >= 41)
+    document.write("Cold weather");
+
+else if (temp < 61 && temp >= 51)
+    document.write("It\'s nice today!");
+
+else if (temp >= 61)
+    document.write("It\'s warm today!");
+    
