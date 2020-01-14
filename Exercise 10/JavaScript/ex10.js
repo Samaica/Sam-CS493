fbcb865fa2f199a106c7aca502f116c1a158f9e1
Sam <47643081+Samaica@users.noreply.github.com>
2020-01-14 10:02:54 -0500

    Create ex10.js

---
+++ Exercise 10/JavaScript/ex10.js
@@ -0,0 +1,35 @@
+console.log("sdkfjdsk");
+
+// let aVar = '5';
+
+// if (aVar == '5');
+// console.log(typeof(aVar)); //string
+
+// if (aVar == '5')
+//     console.log("aVar == '5' YES");
+
+// if (aVar == 5)
+//     console.log("aVar == 5 YES");
+
+// //strict equality operator, checks the type
+// if (aVar === 5)
+//     console.log("aVar === 5 YES")
+
+// if (parseInt(aVar) === 5)
+//     console.log("aVar === 5 YES")
+
+// for (i = 1, j = 10; i <= j; i++)
+//     console.log(i);
+
+// // for (i = 5, count = 100; i <= count; i+5)
+// //     console.log(i);
+
+// max = 2048
+// for (i = 1; i = max; i = i * 2)
+//     console.log(i);
+
+// let listName = ['Kevin', 'Bryan', 'Sam', 'Theodore','Nahid'];
+// console.log(listName[0]);
+
+// for (i = 0; i < listName.length; i++)
+//     console.log(listName[i]);
