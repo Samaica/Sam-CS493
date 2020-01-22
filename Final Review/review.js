0340e6967f0161bb36a5d51133544e6ebc867cd9
Sam <47643081+Samaica@users.noreply.github.com>
2020-01-22 11:26:03 -0500

    Create review.js

---
+++ Final Review/review.js
@@ -0,0 +1,15 @@
+let temp = [
+    {
+        name : "John",
+        age : 12
+    },
+    {
+        name : "Sarah",
+        age : 14
+    }
+];
+
+for (let n of temp) {
+
+    console.log(n.name + " is " + n.age + " years old.\n");
+}
